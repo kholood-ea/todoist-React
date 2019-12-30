e7a87c24614d23e1d6213ae179caa5515c801139
kholood ea <Kholood_Ea@hotmail.com>
2019-12-30 22:14:09 +0200

    Updates

---
+++ precache-manifest.e308e2ace3e1c7deae28544e6066b94f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9e56515453e443882ef3f26204f39c",
+    "url": "/todoist/index.html"
+  },
+  {
+    "revision": "c1ad6a35f21764b2c56c",
+    "url": "/todoist/static/css/main.082aaa52.chunk.css"
+  },
+  {
+    "revision": "403b9c6060fc48a607d7",
+    "url": "/todoist/static/js/2.65429747.chunk.js"
+  },
+  {
+    "revision": "c29216ace69b78af468bfd0f329b39f1",
+    "url": "/todoist/static/js/2.65429747.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1ad6a35f21764b2c56c",
+    "url": "/todoist/static/js/main.4ed2373d.chunk.js"
+  },
+  {
+    "revision": "4341f83e2cb50ff39f1f",
+    "url": "/todoist/static/js/runtime-main.7b61c51b.js"
+  }
+]);
